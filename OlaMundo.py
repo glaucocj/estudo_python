3731a9c00736710e8246fe232fbf56883e20e87f
Glauco Conceição de Jesus <glauco.cj@gmail.com>
2021-12-19 10:13:02 -0300

    Update OlaMundo.py

--- OlaMundo.py
+++ OlaMundo.py
@@ -5,3 +5,17 @@ Comentário de Várias Linhas
 """
 
 print("Olá Mundo!") #Comentário em qualquer linha
+
+#Declaração de Variáveis
+
+a = 1
+b = "Arte"
+c = True
+d = False
+e = 3
+f = a + e
+
+if a >= e:
+	print(e)
+else:
+	print(d)
