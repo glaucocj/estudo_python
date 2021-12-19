e913bca03b384594a6dca17e0b1aef28e92a7d60
Glauco Conceição de Jesus <glauco.cj@gmail.com>
2021-12-19 11:25:29 -0300

    Update Strings Básico.py

--- Strings Básico.py
+++ Strings Básico.py
@@ -1,6 +1,16 @@
 texto1 = "1"
 texto2 = "2"
+texto3 = "Casa"
+texto4 = "Roupa"
 
 concatenar = texto1 + texto2
 
 print (concatenar)
+print (concatenar[0])
+print (texto3[0:2]) #Imprimir uma quantidade limitada de dados [Início:Término]
+
+#Métodos 
+#string.find("string") Busca "string" dentro da string e retorna a posição em int; Retorna -1 caso não encontre
+#string.replace("string1","string2") Altera a "string1" pela "string2" 
+#string.strip() Remove caractere especial
+#string.split("string") Converte a sequência em uma lista usando "string" como divisor
