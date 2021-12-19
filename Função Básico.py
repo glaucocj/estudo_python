9f387499cf3b9ae9633cb5c07d023e56a83228c9
Glauco Conceição de Jesus <glauco.cj@gmail.com>
2021-12-19 11:47:09 -0300

    Update Função Básico.py

--- Função Básico.py
+++ Função Básico.py
@@ -1,4 +1,14 @@
+# -*- coding: UTF-8 -*-
+
 def SOMA(a,b):
 	print("Somando os Valores:",a,"+",b,"=",a+b)
+	return a+b #caso seja necessário armazenar o resultado de uma função ou usa-lo de outra forma
 
 SOMA(5,6)
+
+arquivo = open("arquivo.txt")
+
+linhas =  arquivo.readlines()
+
+for linha in linhas:
+	print(linha)
