ffdb5e07abf44cb5428f76173f6ffbe19cbdb8c0
Glauco Conceição de Jesus <glauco.cj@gmail.com>
2021-12-20 12:06:20 -0300

    Create Listas.py

---
+++ Listas.py
@@ -0,0 +1,19 @@
+lista1 = [1,2,3,4,5,6]
+lista2 = ["a","b","c","d","e"]
+
+print (lista1[3])
+
+for item in lista2:
+	print (item)
+
+tamanho_da_lista = len(lista2)
+
+print (tamanho_da_lista)
+
+lista1.append(7) #adiciona um item a lista
+
+print (lista1)
+
+if "d" in lista2:
+	print ("Está na lista")
+else: print ("Não está na lista")
