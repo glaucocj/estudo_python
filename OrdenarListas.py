1ef60b34e5436fb47cfa34c48dd93739890c74f0
Glauco Conceição de Jesus <glauco.cj@gmail.com>
2021-12-20 13:26:10 -0300

    Create OrdenarListas.py

---
+++ OrdenarListas.py
@@ -0,0 +1,10 @@
+lista = [12,58,35,31,1,8,9,7,325,54,67,98,0,33,3334,58,74]
+
+lista.sort() #aplica a ordenação a lista
+
+print(lista)
+
+lista_ordenada = sorted(lista,reverse=True) #Ordena a lista e possibilita o retorno em uma váriavel
+#reverse=True muda a ordenação
+
+print(lista_ordenada)
