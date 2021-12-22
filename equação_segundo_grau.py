4026e504b456b2adeaf754c59e119c5d342cbade
Glauco Conceição de Jesus <glauco.cj@gmail.com>
2021-12-22 16:10:13 -0300

    Create equação_segundo_grau.py

---
+++ equação_segundo_grau.py
@@ -0,0 +1,14 @@
+print ("Equação do Segundo Grau\nFormato: ax² + bx + c = 0")
+
+##a = int(input("Digite o valor de A: "))
+##b = int(input("Digite o valor de B: "))
+##c = int(input("Digite o valor de C: "))
+
+a = 2
+b = -3
+c = -5
+
+print(a,"x² + ",b,"x + ",c,"= 0")
+
+delta = b**2-4*a*c
+print (delta)
